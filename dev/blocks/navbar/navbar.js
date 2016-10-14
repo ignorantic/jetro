e0558d743b1cd552e5659c207e0d28279eb8a2f3
ignorantic <ansolaris@gmail.com>
2016-10-15 01:18:18 +0300

    Remove jQuery

---
+++ dev/blocks/navbar/navbar.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+  
+    document.querySelector('.navbar__btn').onclick = function () {
+        var btn = document.querySelector('.navbar__btn'),
+            list = document.querySelector('.navbar__list');
+        btn.classList.add('navbar__btn_blink');
+        list.classList.toggle('navbar__list_active');
+        setTimeout(function() {btn.classList.remove('navbar__btn_blink')}, 300);
+    };
+
+});
