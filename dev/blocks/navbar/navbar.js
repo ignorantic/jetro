e227adadca828a8ccc4fe12314ac4f7cde8d3b84
ignorantic <ansolaris@gmail.com>
2016-10-25 12:55:28 +0300

    pug #5 added blog paginator

--- dev/blocks/navbar/navbar.js
+++ dev/blocks/navbar/navbar.js
@@ -1,17 +1,23 @@
 if (document.querySelector('.navbar') != null) {
 
     (function addActiveLink() {
-        let address, index, page, links, set, link;
+        let address, index, lastIndex, page, links, set, link;
         address = location.href;
-        index = address.match(/[a-z][a-z0-9]*\.html$/i);
-        if (index != null) page = index;
-        else page = 'index.html';
+        index = address.match(/\/[a-z][a-z0-9]*[\/\.]/ig);
+        if (index == null) page = '/index';
+        else {
+            lastIndex = index[index.length - 1].slice(0, -1);
+            page = lastIndex;
+        }
+        console.log('page: ' + page);
         links = document.querySelectorAll('.navbar__link');
         for (let i = 0; i < links.length; i++) {
             set = links[i].toString();
-            link = set.match(/[a-z][a-z0-9]*\.html$/i);
-            if (link != null) {
+            link = set.match(/[\/][a-z][a-z0-9]*[\/\.]/i);
+            link[0] = link[0].slice(0, -1);
+            if (link[0] != null) {
                 if (link[0] == page) {
+                    console.log('link: ' + link);
                     toggleClassByIndex('navbar__link', 'navbar__link_active', i);
                 }
             }
